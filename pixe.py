890ec3b3871ad01df64821429c3340f4554f0e50
FirepantsCZ <paolozisa24@gmail.com>
2021-04-14 22:50:21 +0200

    Fixed reconnecting

--- pixe.py
+++ pixe.py
@@ -7,6 +7,7 @@ import pyautogui
 import keyboard
 import PyQt5
 from PIL import Image
+from PIL import ImageChops
 from PyQt5 import QtWidgets, QtCore, QtWidgets
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -17,6 +18,9 @@ driver.find_element_by_xpath('//button[normalize-space()="OK"]').click()
 
 loadi = int(json.loads(open("pixe.json", "r").read())[0].get("i"))
 loadj = int(json.loads(open("pixe.json", "r").read())[0].get("j"))
+lastfile = json.loads(open("pixe.json", "r").read())[0].get("lastfile")
+customsize = json.loads(open("pixe.json", "r").read())[0].get("customsize")
+
 print(str(loadi) + " " + str(loadj))
 if loadi != 0 or loadj != 0:
     ans = input("Pokračovat? [Y/N]")
@@ -56,48 +60,71 @@ class App(PyQt5.QtWidgets.QWidget):
         self.hide()
 
     def initUI(self):
+        global fileName
+        global lastfile
+        global customsize
 
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        self.openFileNameDialog()
-        self.show()
-
-    def openFileNameDialog(self):
-        options = PyQt5.QtWidgets.QFileDialog.Options()
-        options |= PyQt5.QtWidgets.QFileDialog.DontUseNativeDialog
-        fileName, _ = PyQt5.QtWidgets.QFileDialog.getOpenFileName(
-            self, "Choose file", "", "Image Files (*.png *.jpg);;All Files (*)", options=options)
-        if fileName:
-            print(fileName)
-
-        image = Image.open(fileName)
-        print(image.size)
-        cust = input("Vlastní velikost? [Y/N]")
-        if cust == "y" or cust == "Y":
-            crust = input("Udržet poměr stran? [Y/N]")
-            if crust == "y" or crust == "Y":
-                usersizewidth = int(input("Image width (owop pixels): "))
-                usersizeheight = int(input("Image height (owop pixels): "))
-                image.thumbnail((usersizewidth, usersizeheight))
-            else:
-                usersizewidth = int(input("Image width (owop pixels): "))
-                usersizeheight = int(input("Image height (owop pixels): "))
-                image.resize(usersizewidth, usersizeheight)
-        if image.size > (400, 300):
-            print("image too larege, resizing!")
-            crust = input("Udržet poměr stran? [Y/N]")
-            if crust == "y" or crust == "Y":
-                new_image = image.thumbnail((400, 300))
-            else:
-                new_image = image.resize((400, 300))
-            new_image.save("botimage.png")
-        else:
-            print("image size smaller, not resizing...")
-            image.save("botimage.png")
-        image.close()
-
         while True:
+            if contmode:
+                fileName = lastfile
+            else:
+                fileName = self.openFileNameDialog()
+                self.hide()
+            lastfile = fileName
+            image = Image.open(fileName)
+            print(image.size)
+            if not contmode:
+                cust = input("Vlastní velikost? [Y/N]")
+                if cust == "y" or cust == "Y":
+                    crust = input("Udržet poměr stran? [Y/N]")
+                    if crust == "y" or crust == "Y":
+                        usersizewidth = int(
+                            input("Image width (owop pixels): "))
+                        usersizeheight = int(
+                            input("Image height (owop pixels): "))
+                        image.thumbnail((usersizewidth, usersizeheight))
+                    else:
+                        usersizewidth = int(
+                            input("Image width (owop pixels): "))
+                        usersizeheight = int(
+                            input("Image height (owop pixels): "))
+                        image.resize(usersizewidth, usersizeheight)
+                    customsize = (usersizewidth, usersizeheight)
+                else:
+                    customsize = "none"
+                    image.save("botimage.png")
+            else:
+                if customsize != "none":
+                    custx, custy = tuple(customsize)
+                    print(tuple(customsize))
+                    botimage = image.resize(tuple(customsize))
+                    print("Found previous custom size, resizing to " +
+                          str(customsize))
+                    botimage.save("botimage.png")
+                    botimage.close()
+                else:
+                    print(
+                        "No custom size found, defaulting to original size" + str(image.size))
+                    image.save("botimage.png")
+
+            """if image.size > (400, 300):
+                print("image too larege, resizing!")
+                crust = input("Udržet poměr stran? [Y/N]")
+                if crust == "y" or crust == "Y":
+                    new_image = image.thumbnail((400, 300))
+                else:
+                    new_image = image.resize((400, 300))
+                new_image.save("botimage.png")
+            else:
+                print("image size smaller, not resizing...")
+                image.save("botimage.png")"""
+            # botimage.save("botimage.png")
+            print(image.size)
+            image.close()
+            #global lastfile
             global scrollmode
             global markMode
             global oldcol
@@ -145,16 +172,26 @@ class App(PyQt5.QtWidgets.QWidget):
                 else:
                     vertpos = (marky + maxmarky) + 2
                     horpos = (markx + maxmarkx) + 2
+                i = loadi
                 for i in range(loadi, imageheight):
                     pyautogui.moveTo(horpos, vertpos)
-                    for j in range(loadj, imagewidth):
+                    j = loadj
+                    while j < imagewidth:
+                        # for j in range(loadj, imagewidth):
+                        currentMouseX, currentMouseY = pyautogui.position()
                         reconnect = driver.find_element_by_id('load-options')
                         vis = reconnect.get_attribute("class")
                         print("Class is: " + vis)
                         if vis == "framed":
-                            driver.find_element_by_id("reconnect-btn").click()                        
-                            time.sleep(1.5)
-                        currentMouseX, currentMouseY = pyautogui.position()
+                            driver.find_element_by_id("reconnect-btn").click()
+                            time.sleep(5)
+                            pyautogui.click()
+                            print("clicked")
+                            time.sleep(1)
+                            pyautogui.click()
+                            print("clicked")
+                            #j -= 2
+                            #pyautogui.moveTo(currentMouseX - 6, currentMouseY)
                         print(i, j)
                         r, g, b = image.getpixel((j, i))
                         print(r + g + b)
@@ -172,22 +209,92 @@ class App(PyQt5.QtWidgets.QWidget):
                             keyboard.press_and_release("enter")
                             #time.sleep(0.5)"""
                         pyautogui.moveTo(currentMouseX + 3, currentMouseY)
+                        if vis == "framed":
+                            try:
+                                driver.find_element_by_id(
+                                    "reconnect-btn").click()
+                                time.sleep(5)
+                                pyautogui.click()
+                                print("clicked")
+                                time.sleep(1)
+                                pyautogui.click()
+                                print("clicked")
+                            except:
+                                print("button not on screen")
                         pyautogui.click()
+                        if vis == "framed":
+                            try:
+                                driver.find_element_by_id(
+                                    "reconnect-btn").click()
+                                time.sleep(5)
+                                pyautogui.click()
+                                print("clicked")
+                                time.sleep(1)
+                                pyautogui.click()
+                                print("clicked")
+                            except:
+                                print("button not on screen")
                         oldcol = hexcol
                         oldr = r
                         oldg = g
                         oldb = b
-                        jdump = [{"i": i, "j": j}]
+                        jdump = [
+                            {"i": i, "j": j, "lastfile": lastfile, "customsize": customsize}]
                         print(jdump)
                         open("pixe.json", "w").write(json.dumps(jdump))
                         # time.sleep(0.5)
+                        j += 1
                     loadj = 0
                     horpos = (markx + maxmarkx) + 2
                     vertpos += 3
+                    jdump = [{"i": 0, "j": 0, "lastfile": lastfile,
+                              "customsize": customsize}]
+                    print(jdump)
+                    open("pixe.json", "w").write(json.dumps(jdump))
                 print("Finished drawing!")
-                sys.exit()
+                print("Press s to capture differences")
+                keyboard.wait("s")
+                keyboard.press_and_release("g")
+                image1 = pyautogui.screenshot("finished.png", region=(markx + maxmarkx + 3, marky + maxmarky, imagewidth * 3, imageheight * 3)).convert("RGB").resize((10, 10))
+                image2 = Image.open("botimage.png").convert("RGB")
+                print(image1.size)
+                print(image2.size)
+                diff = ImageChops.difference(image1, image2)
+                diff.save("diff.png")
+                print(diff.mode)
+                compwidth, compheight = image1.size
+                for i in range(compheight):
+                    for j in range(compwidth):
+                        print(str(i) + " " + str(j))
+                        r1, g1, b1 = image1.getpixel((j,i))
+                        r2, g2, b2 = image2.getpixel((j,i))
+                        rdif = abs(r1 - r2)
+                        gdif = abs(g1 - g2)
+                        bdif = abs(b1 - b2)
 
+                        r, g, b = diff.getpixel((j,i))
+                        brightness = r + g + b
+                        print("red: " + str(r))
+                        print("green: " + str(g))
+                        print("blue: " + str(b))
+                        print(brightness)
+                        print("red difference: " + str(rdif))
+                        print("green difference: " + str(gdif))
+                        print("blue difference: " + str(bdif))
+                diff.close()
+                image1.close()
+                image2.close()
+                sys.exit()
             time.sleep(0.05)
+
+    def openFileNameDialog(self):
+        options = PyQt5.QtWidgets.QFileDialog.Options()
+        options |= PyQt5.QtWidgets.QFileDialog.DontUseNativeDialog
+        fileName, _ = PyQt5.QtWidgets.QFileDialog.getOpenFileName(
+            self, "Choose file", "", "Image Files (*.png *.jpg);;All Files (*)", options=options)
+        if fileName:
+            print(fileName)
+            return fileName
 
 
 def mousestat():
@@ -198,7 +305,7 @@ def mousestat():
         showmousepos = False
 
 
-keyboard.add_hotkey('g', lambda: os.kill(os.getpid(), signal.SIGINT))
+keyboard.add_hotkey('h', lambda: os.kill(os.getpid(), signal.SIGINT))
 keyboard.add_hotkey('shift', lambda: mousestat())
 
 # scroll 13x
